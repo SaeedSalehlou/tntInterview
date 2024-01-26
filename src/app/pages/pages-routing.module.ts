7e767923f236ecfcdec07107430c4e24f1f65d31
saeed salehlou <saeed.softco@gmail.com>
2024-01-26 19:13:15 +0330

    fix pages route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PagesComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
